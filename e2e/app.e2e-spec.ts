2e5c94e6ff65eed259b06c297c027a4bd1c210d9
Tuomas Talus <talus.tuomas@gmail.com>
2018-01-23 20:30:10 +0200

    e2e test updated

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('art-gallery App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Art collection');
   });
 });
